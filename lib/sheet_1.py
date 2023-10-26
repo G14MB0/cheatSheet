3a892f85c9f8c08adba0ee7998031893d4389629
G14MB0 <castaldini.gianmaria@gmail.com>
2023-10-26 19:27:36 +0200

    1

--- lib/sheet_1.py
+++ lib/sheet_1.py
@@ -33,3 +33,49 @@ def readCsvAsString(filePath, sep=","):
     return df
 
 
+
+
+import smtplib
+from email.mime.text import MIMEText
+def send_email(subject, message, to_email):
+    """invia email con gmail
+
+    Important: To create an app password, you need 2-Step Verification on your Google Account.
+
+    If you use 2-Step-Verification and get a "password incorrect" error when you sign in, you can try to use an app password.
+
+    Go to your Google Account.
+    Select Security.
+    Under "Signing in to Google," select 2-Step Verification.
+    At the bottom of the page, select App passwords.
+    Enter a name that helps you remember where you’ll use the app password.
+    Select Generate.
+    To enter the app password, follow the instructions on your screen. The app password is the 16-character code that generates on your device.
+    Select Done.
+    If you’ve set up 2-Step Verification but can’t find the option to add an app password, it might be because:
+
+    Your Google Account has 2-Step Verification set up only for security keys.
+    You’re logged into a work, school, or another organization account.
+    Your Google Account has Advanced Protection.
+
+    Args:
+        subject (_type_): _description_
+        message (_type_): _description_
+        to_email (_type_): _description_
+    """    
+    from_email = "project.vpp@gmail.com"
+    password = "qedq awim kmsp nccx"
+
+    msg = MIMEText(message)
+    msg['Subject'] = subject
+    msg['From'] = from_email
+    msg['To'] = to_email
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(from_email, password)
+    server.sendmail(from_email, to_email, msg.as_string())
+    server.quit()
+
+# send_email("Test Subject", "This is the body of the email.", "castaldini.gianmaria@gmail.com")
+
